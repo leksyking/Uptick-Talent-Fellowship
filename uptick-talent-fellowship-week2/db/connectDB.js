baac6ad6fc423f2e7e846831f3d58bcf7e6f946f
leksyking <foogundipe@student.oauife.edu.ng>
2023-10-20 04:19:51 +0100

    implementaton of load balancer being added

--- uptick-talent-fellowship-week2/db/connectDB.js
+++ uptick-talent-fellowship-week2/db/connectDB.js
@@ -1,6 +1,6 @@
 const { connect, set } = require("mongoose");
 
 module.exports = function connectDB(url) {
-    set("strictQuery", true);
-    return connect(url);
+        set("strictQuery", true);
+        return connect(url);
 };
